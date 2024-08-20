c74b15e8b1837360fc599d3da21927c4f84cbd6a
SooryaKumarP <sooryakumar1997@gmail.com>
2024-08-20 16:08:40 +0530

    sd card pins and imu2 bmi088

---
+++ KILLIS_FIRMWARES/KILLIS_TEST_6/ap_version.h
@@ -0,0 +1,12 @@
+// auto-generated header, do not edit
+
+#pragma once
+
+#ifndef FORCE_VERSION_H_INCLUDE
+#error ap_version.h should never be included directly. You probably want to include AP_Common/AP_FWVersion.h
+#endif
+
+#define GIT_VERSION "0fa849e3"
+#define GIT_VERSION_INT 262687203
+#define AP_BUILD_ROOT "/home/killis01/ardupilot"
+#define CHIBIOS_GIT_VERSION "cc37d7f2"
